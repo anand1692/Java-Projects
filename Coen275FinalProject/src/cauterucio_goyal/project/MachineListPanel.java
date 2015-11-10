fa748e508a39805a62d16708aeb32a6a60791130
SCauterucio <sfgman2@gmail.com>
2015-11-09 23:26:48 -0800

    RecyclingStation GUI update
    RecyclingStation GUI can now display multiple machines - each having
    buttons for activation, removal, modification, and viewing statistics.

--- Coen275FinalProject/src/cauterucio_goyal/project/MachineListPanel.java
+++ Coen275FinalProject/src/cauterucio_goyal/project/MachineListPanel.java
@@ -24,6 +24,8 @@ public class MachineListPanel extends JPanel {
 	
 	public MachineListPanel () {
 		
+		int i=0;
+		
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		
@@ -34,77 +36,62 @@ public class MachineListPanel extends JPanel {
 		c.fill = GridBagConstraints.CENTER;
 		this.add(titleLabel, c);
 		
+		// currently hardcoded to display 10 machines. This will change once we know how many active
+		// machines there are.
+		for (i = 1; i <= 20; i = i+2) {
+			
+			machineIdLabel = new JLabel("Machine " + i/2);
+			machineIdLabel.setFont(new Font("Arial", Font.PLAIN, 24));
+			c.gridx = 0;
+			c.gridy = i;
+			c.weightx = 0.4;
+			c.gridheight = 1;
+			c.fill = GridBagConstraints.HORIZONTAL;
+			this.add(machineIdLabel, c);
+			
+			machineLocationLabel = new JLabel("        <Location>");
+			machineLocationLabel.setFont(new Font("Arial", Font.PLAIN, 24));
+			c.gridx = 0;
+			c.gridy = i+1;
+			c.weightx = 0.4;
+			this.add(machineLocationLabel, c);
 		
-		machineIdLabel = new JLabel("Machine <ID>");
-		machineIdLabel.setFont(new Font("Arial", Font.PLAIN, 24));
-		c.gridx = 0;
-		c.gridy = 1;
-		c.weightx = 0.4;
-		c.fill = GridBagConstraints.HORIZONTAL;
-		this.add(machineIdLabel, c);
+			c.fill = GridBagConstraints.CENTER;
+			
+			//c.insets = new Insets(0, 10, 10, 0);
+			activationButton = new JButton("Activate");
+			activationButton.setToolTipText("Activate this machine");
+			c.gridx = 1;
+			c.gridy = i;
+			c.gridheight = 2;
+			c.weightx = 0.15;
+			this.add(activationButton, c);
 		
-		machineLocationLabel = new JLabel("        <Location>");
-		machineLocationLabel.setFont(new Font("Arial", Font.PLAIN, 24));
-		c.gridx = 0;
-		c.gridy = 2;
-		c.weightx = 0.4;
-		this.add(machineLocationLabel, c);
-		
-		c.fill = GridBagConstraints.CENTER;
-		
-		//c.insets = new Insets(0, 10, 10, 0);
-		activationButton = new JButton("Activate");
-		activationButton.setToolTipText("Activate this machine");
-		c.gridx = 1;
-		c.gridy = 1;
-		c.gridheight = 2;
-		c.weightx = 0.15;
-		this.add(activationButton, c);
-		
-		removeMachineButton = new JButton("Remove");
-		removeMachineButton.setToolTipText("Remove this machine from the Recycling Station");
-		c.gridx = 2;
-		c.gridy = 1;
-		c.gridheight = 2;
-		c.weightx = 0.15;
-		this.add(removeMachineButton, c);
-		
-		modifyMachineButton = new JButton("Modify");
-		modifyMachineButton.setToolTipText("Modify this machine's settings");
-		c.gridx = 3;
-		c.gridy = 1;
-		c.gridheight = 2;
-		c.weightx = 0.15;
-		this.add(modifyMachineButton, c);
-		
-		viewStatsButton = new JButton("View Stats");
-		viewStatsButton.setToolTipText("View statistics about this machine");;
-		c.gridx = 4;
-		c.gridy = 1;
-		c.gridheight = 2;
-		c.weightx = 0.15;
-		//c.insets = new Insets(0, 10, 10, 0);
-		this.add(viewStatsButton, c);
-
-		/*
-		JButton button;
-button = new JButton("Button 2");
-c.fill = GridBagConstraints.HORIZONTAL;
-c.weightx = 0.5;
-c.gridx = 0;
-c.gridy = 1;
-this.add(button, c);
-
-button = new JButton("Button 3");
-c.fill = GridBagConstraints.HORIZONTAL;
-c.weightx = 0.5;
-c.gridx = 1;
-c.gridy = 2;
-this.add(button, c);
-
-c.insets = new Insets(0, 0, 0, 0);
-
-*/
+			removeMachineButton = new JButton("Remove");
+			removeMachineButton.setToolTipText("Remove this machine from the Recycling Station");
+			c.gridx = 2;
+			c.gridy = i;
+			c.gridheight = 2;
+			c.weightx = 0.15;
+			this.add(removeMachineButton, c);
+			
+			modifyMachineButton = new JButton("Modify");
+			modifyMachineButton.setToolTipText("Modify this machine's settings");
+			c.gridx = 3;
+			c.gridy = i;
+			c.gridheight = 2;
+			c.weightx = 0.15;
+			this.add(modifyMachineButton, c);
+			
+			viewStatsButton = new JButton("View Stats");
+			viewStatsButton.setToolTipText("View statistics about this machine");;
+			c.gridx = 4;
+			c.gridy = i;
+			c.gridheight = 2;
+			c.weightx = 0.15;
+			//c.insets = new Insets(0, 10, 10, 0);
+			this.add(viewStatsButton, c);
+		}
 
 	}
 }
