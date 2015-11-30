72c3881e877fd830986b3ade2e3a286635d7229d
Anand Goyal <anand.goyal007@gmail.com>
2015-11-29 19:41:11 -0800

    Create MachineStatus.java
    This has all the statistical attributes about the machine which can used to maintain and store all kinds of statistics required.

---
+++ MachineStatus.java
@@ -0,0 +1,187 @@
+package goyal.project;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.TreeMap;
+
+public class MachineStatus {
+	
+	double moneyInMachine; 	// in $
+	double couponsInMachine;	// worth in $
+	double weightInMachine;	// in tons
+	private TreeMap<Date, Double> emptyTimestamp;
+	int numberOfTimesEmptied;
+	double weightCapacity; 	// in pounds
+	int totalItemsCollected;
+	private TreeMap<String, Integer> itemsCollectedByType;
+	double totalCashIssued;
+	double totalCouponsIssued;
+	
+	public MachineStatus(ArrayList<String> recyclableItems) {
+		// TODO Auto-generated constructor stub
+		moneyInMachine = 1000;
+		couponsInMachine = 1000;
+		weightInMachine = 0;
+		numberOfTimesEmptied = 0;
+		weightCapacity = 1000; 
+		totalItemsCollected = 0;
+		totalCashIssued = 0;
+		totalCouponsIssued = 0;
+		
+		emptyTimestamp = new TreeMap<Date, Double>();
+		
+		itemsCollectedByType = new TreeMap<String, Integer>();
+		for(String s: recyclableItems) {
+			itemsCollectedByType.put(s, 0);
+		}
+	}
+	
+	/**
+	 * @return the moneyInMachine
+	 */
+	public double getMoneyInMachine() {
+		return moneyInMachine;
+	}
+
+	/**
+	 * @return the couponsInMachine
+	 */
+	public double getCouponsInMachine() {
+		return couponsInMachine;
+	}
+
+	/**
+	 * @return the weightInMachine
+	 */
+	public double getWeightInMachine() {
+		return weightInMachine;
+	}
+
+	/**
+	 * @return the emptyTimestamp
+	 */
+	public TreeMap<Date, Double> getEmptyTimestamp() {
+		return emptyTimestamp;
+	}
+
+	/**
+	 * @return the numberOfTimesEmptied
+	 */
+	public int getNumberOfTimesEmptied() {
+		return numberOfTimesEmptied;
+	}
+
+	/**
+	 * @return the weightCapacity
+	 */
+	public double getWeightCapacity() {
+		return weightCapacity;
+	}
+
+	/**
+	 * @return the totalItemsCollected
+	 */
+	public int getTotalItemsCollected() {
+		return totalItemsCollected;
+	}
+
+	/**
+	 * @return the itemsCollectedByType
+	 */
+	public TreeMap<String, Integer> getItemsCollectedByType() {
+		return itemsCollectedByType;
+	}
+
+	/**
+	 * @return the totalCashIssued
+	 */
+	public double getTotalCashIssued() {
+		return totalCashIssued;
+	}
+
+	/**
+	 * @return the totalCouponsIssued
+	 */
+	public double getTotalCouponsIssued() {
+		return totalCouponsIssued;
+	}
+	
+	/**
+	 * @return the totalValueIssued
+	 */
+	public double getTotalValueIssued() {
+		return (double)totalCouponsIssued + totalCashIssued;
+	}
+	
+	/**
+	 * @param moneyInMachine the moneyInMachine to set
+	 */
+	public void setMoneyInMachine(double moneyInMachine) {
+		this.moneyInMachine = moneyInMachine;
+	}
+
+	/**
+	 * @param couponsInMachine the couponsInMachine to set
+	 */
+	public void setCouponsInMachine(double couponsInMachine) {
+		this.couponsInMachine = couponsInMachine;
+	}
+
+	/**
+	 * @param weightInMachine the weightInMachine to set
+	 */
+	public void setWeightInMachine(double weightInMachine) {
+		this.weightInMachine = weightInMachine;
+	}
+
+	/**
+	 * @param emptyTimestamp the emptyTimestamp to set
+	 */
+	public void setEmptyTimestamp(TreeMap<Date, Double> emptyTimestamp) {
+		this.emptyTimestamp = emptyTimestamp;
+	}
+
+	/**
+	 * @param numberOfTimesEmptied the numberOfTimesEmptied to set
+	 */
+	public void setNumberOfTimesEmptied(int numberOfTimesEmptied) {
+		this.numberOfTimesEmptied = numberOfTimesEmptied;
+	}
+
+	/**
+	 * @param weightCapacity the weightCapacity to set
+	 */
+	public void setWeightCapacity(double weightCapacity) {
+		this.weightCapacity = weightCapacity;
+	}
+
+	/**
+	 * @param totalItemsCollected the totalItemsCollected to set
+	 */
+	public void setTotalItemsCollected(int totalItemsCollected) {
+		this.totalItemsCollected = totalItemsCollected;
+	}
+
+	/**
+	 * @param itemsCollectedByType the itemsCollectedByType to set
+	 */
+	public void setItemsCollectedByType(String item) {
+		TreeMap<String, Integer> itemsCollected = this.getItemsCollectedByType();
+		itemsCollected.put(item, itemsCollected.get(item)+1);	
+		this.itemsCollectedByType = itemsCollected;
+	}
+
+	/**
+	 * @param totalCashIssued the totalCashIssued to set
+	 */
+	public void setTotalCashIssued(double totalCashIssued) {
+		this.totalCashIssued = totalCashIssued;
+	}
+
+	/**
+	 * @param totalCouponsIssued the totalCouponsIssued to set
+	 */
+	public void setTotalCouponsIssued(double totalCouponsIssued) {
+		this.totalCouponsIssued = totalCouponsIssued;
+	}
+}
